4df9214ffbbad2842a47dc06b35bb0a91b586079
Vladislav <81467635+grGred@users.noreply.github.com>
2022-08-25 13:10:33 +0300

    update config script

--- scripts/setupCrossChain.ts
+++ scripts/setupCrossChain.ts
@@ -6,15 +6,16 @@ async function main() {
     const proxyFactory = await ethers.getContractFactory('RubicProxy');
 
     let proxy = (await proxyFactory.attach(
-        '0x3332241a5a4eCb4c28239A9731ad45De7f000333'
+        '0x3335A88bb18fD3b6824b59Af62b50CE494143333'
     )) as RubicProxy;
 
-    await proxy.setRubicPlatformFee(0);
+    // await proxy.setRubicPlatformFee(0);
     await proxy.grantRole(
         '0x241ecf16d79d0f8dbfb92cbc07fe17840425976cf0667f022fe9877caa831b08',
-        '0x30a47a58459405dA8fE5cdDDaAcE66Ba005423F1'
+        '0x8F53A5BDE40F809958f5a0A568B7D65A4EB07349'
     );
-    await proxy.setFixedCryptoFee('8333300000000000000');
+    console.log('Role granted');
+    // await proxy.setFixedCryptoFee('8333300000000000000');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
