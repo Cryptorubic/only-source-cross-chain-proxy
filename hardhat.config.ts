93f3a24692df5d124d038a5bcffe4d0aab5744f1
debych <zh.eliseev@yandex.ru>
2022-11-29 14:17:46 +0300

    update compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,11 +12,11 @@ const DEFAULT_PRIVATE_KEY =
     process.env.MNEMONIC || '1000000000000000000000000000000000000000000000000000000000000000';
 
 const DEFAULT_COMPILER_SETTINGS: SolcUserConfig = {
-    version: '0.8.15',
+    version: '0.8.16',
     settings: {
         optimizer: {
             enabled: true,
-            runs: 200
+            runs: 10_000
         },
         metadata: {
             bytecodeHash: 'none'
